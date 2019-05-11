e7a0f219ce5c89dfc8d6bea95a491bb3b52ccaa9
Peter Motschmann <pnm3@optonline.net>
2019-05-11 07:23:52 -0400

    University base price
    Decreased the starting cost of university slightly.

--- vars.js
+++ vars.js
@@ -53,7 +53,7 @@ else {
     newGameData();
 }
 
-global['version'] = '0.2.50';
+global['version'] = '0.2.51';
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
