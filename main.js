7cf5d5eaf06fe0c680ae12d1c982cf794fc7c924
Peter Motschmann <pnm3@optonline.net>
2019-05-18 11:45:19 -0400

    Removing unneeded loop
    Removing unused code that was accidently left in.

--- main.js
+++ main.js
@@ -324,27 +324,24 @@ function fastLoop(){
         global_multiplier += plasmidBonus();
     }
 
-    let bd_cats = ['consume','time'];
-    for (let i=0; i<bd_cats.length; i++){
-        breakdown.p[bd_cats[i]] = {
-            Money: {},
-            Knowledge: {},
-            Food: {},
-            Lumber: {},
-            Stone: {},
-            Furs: {},
-            Copper: {},
-            Iron: {},
-            Cement: {},
-            Coal: {},
-            Oil: {},
-            Uranium: {},
-            Steel: {},
-            Titanium: {},
-            Alloy: {},
-            Polymer: {}
-        };
-    }
+    breakdown.p['consume'] = {
+        Money: {},
+        Knowledge: {},
+        Food: {},
+        Lumber: {},
+        Stone: {},
+        Furs: {},
+        Copper: {},
+        Iron: {},
+        Cement: {},
+        Coal: {},
+        Oil: {},
+        Uranium: {},
+        Steel: {},
+        Titanium: {},
+        Alloy: {},
+        Polymer: {}
+    };
     
     var time_multiplier = 0.25;
         
