fcc81610e096b0095b40880747dbcfa48045174d
Peter Motschmann <pnm3@optonline.net>
2019-05-26 22:45:11 -0400

    Fixed storage unlocking bug
    Fixed bug which could let you get free barn or warehouse upgrade by essentially skipping over the tech.

--- vars.js
+++ vars.js
@@ -108,7 +108,7 @@ if (convertVersion(global['version']) < 2060){
     });
 }
 
-global['version'] = '0.2.68';
+global['version'] = '0.2.69';
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
