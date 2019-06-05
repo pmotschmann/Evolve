ba9cad591e541efa858c0d981c90e470e9e50670
Peter Motschmann <pnm3@optonline.net>
2019-06-05 08:02:08 -0400

    Updated Version infromation for 0.3.5

--- vars.js
+++ vars.js
@@ -152,7 +152,7 @@ if (convertVersion(global['version']) < 2060){
     });
 }
 
-global['version'] = '0.3.4';
+global['version'] = '0.3.5';
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
