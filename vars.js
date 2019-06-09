3dca6d28f8712df34744072a0c3fbbc94b34bb05
Peter Motschmann <pnm3@optonline.net>
2019-06-09 12:08:39 -0400

    Cache busting
    Automatic file cache busting on version update added to the game.

--- vars.js
+++ vars.js
@@ -152,7 +152,9 @@ if (convertVersion(global['version']) < 2060){
     });
 }
 
-global['version'] = '0.3.8';
+if (version){
+    global['version'] = version;
+}
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
