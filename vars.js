c3db805dce0ece5f2f4ecbba170878a2588e9525
Peter Motschmann <pnm3@optonline.net>
2019-06-09 20:42:26 -0400

    Revert "Cache busting"
    This reverts commit 3dca6d28f8712df34744072a0c3fbbc94b34bb05.

--- vars.js
+++ vars.js
@@ -152,9 +152,7 @@ if (convertVersion(global['version']) < 2060){
     });
 }
 
-if (version){
-    global['version'] = version;
-}
+global['version'] = '0.3.8';
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
