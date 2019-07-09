ca51a2b68822716254258676c228eb59aa52bbe7
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2019-07-09 16:30:50 -0300

    pt-BR translation (617/1993)

--- locale.js
+++ locale.js
@@ -65,5 +65,5 @@ function getString(locale) {
 export const locales = {
     'en-US': 'English (US)',
     //'es-US': 'Spanish (US/Latin-America)',
-    'pt-BR': 'Português Brasileiro [28%]',
+    'pt-BR': 'Português Brasileiro [34%]',
 };
