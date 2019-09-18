177bbf4668bbb5d9bb77a1e3e1abe2b37c830b7e
Peter Motschmann <pnm3@optonline.net>
2019-09-18 07:24:08 -0400

    heavy gravity label update

--- space.js
+++ space.js
@@ -2757,7 +2757,7 @@ export const universe_types = {
     heavy: {
         name: loc('universe_heavy'),
         desc: loc('universe_heavy_desc'),
-        effect: loc('universe_heavy_effect')
+        effect: loc('universe_heavy_effect',[25])
     },
     antimatter: {
         name: loc('universe_antimatter'),
