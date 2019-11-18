c7822580011bb9415f1fdc9172e5a80e5e9256d7
Peter Motschmann <pmotsch3@gmail.com>
2019-11-18 08:05:58 -0500

    Enhanced Spy Data Adding additional spies reveals more specific data.

--- src/vars.js
+++ src/vars.js
@@ -355,7 +355,7 @@ if (convertVersion(global['version']) < 7000){
 }
 
 global['version'] = '0.7.0';
-global['beta'] = '12';
+global['beta'] = '13';
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
