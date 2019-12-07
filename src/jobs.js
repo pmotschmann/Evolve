82b94e52a1bdef1348b0c8fa859c3740965735f4
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2019-12-07 19:08:16 -0300

    localized word 'cycle'

--- src/jobs.js
+++ src/jobs.js
@@ -393,13 +393,14 @@ export function loadFoundry(){
                     }
                     let final = +(global.city.foundry[res] * multiplier).toFixed(2);
                     let bonus = (multiplier * 100).toFixed(0);
+                    let cycle = loc("per_cycle");
 
                     popper.append($(`<div>+${bonus}% Craftsman ${name}</div>`));
-                    popper.append($(`<div>+${final} ${name}/cycle</div>`));
+                    popper.append($(`<div>+${final} ${name}/${cycle}</div>`));
                     let craft_cost = craftCost();
                     for (let i=0; i<craft_cost[res].length; i++){
                         let cost = +(craft_cost[res][i].a * global.city.foundry[res]).toFixed(2);
-                        popper.append($(`<div>-${cost} ${global.resource[craft_cost[res][i].r].name}/cycle<div>`));
+                        popper.append($(`<div>-${cost} ${global.resource[craft_cost[res][i].r].name}/${cycle}<div>`));
                     }
     
                     popper.show();
