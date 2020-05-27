100d12c4d1ef6cd0b3b407e58b7ccb0e6bf42d33
Peter Motschmann <pmotsch3@gmail.com>
2020-05-27 08:37:12 -0400

    Updated beta version

--- src/vars.js
+++ src/vars.js
@@ -430,7 +430,7 @@ if (convertVersion(global['version']) < 8017){
 }
 
 global['version'] = '0.8.17';
-global['beta'] = 1;
+global['beta'] = 2;
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
