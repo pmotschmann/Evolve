e7b7f0bb5cacd034c8cc2b3cec705dc790e81ff7
Peter Motschmann <pmotsch3@gmail.com>
2020-07-30 01:07:00 -0400

    Build release version of 0.9.10

--- src/vars.js
+++ src/vars.js
@@ -469,7 +469,7 @@ if (convertVersion(global['version']) < 9010){
 }
 
 global['version'] = '0.9.10';
-global['beta'] = 2;
+delete global['beta'];
 
 if (global.civic['cement_worker'] && global.civic.cement_worker.impact === 0.25){
     global.civic.cement_worker.impact = 0.4;
