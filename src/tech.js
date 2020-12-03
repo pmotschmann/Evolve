64a03eeb9d5c0847d460a6854c0ff4511e8e7c9d
Peter Motschmann <pmotsch3@gmail.com>
2020-12-02 20:01:03 -0500

    Added more cheese

--- src/tech.js
+++ src/tech.js
@@ -9911,7 +9911,11 @@ function swissKnife(){
         'fontina','reblochon','port_salut','brillat_savarin','cream','stilton','taleggio','manchego','edam','gruyeye',
         'parmigiano_reggiano','epoisses','comte','caciocavallo','teleme','grana_padano','chaumes','morbier','halloumi',
         'lancashire','bleu_de_gex','fourme_d_ambert','fromage_blanc','red_leicester','bleu_d_auvergne','pont_l_eveque',
-        'jl_grubb','castello_blue'
+        'jl_grubb','castello_blue','wagasi','caravane','limburger','herve','kashkaval','sirene','paski_sir','akkawi',
+        'olomoucke_syrecky','anari','danbo','hermelín','trappista','stinking_bishop','banbury','sulguni','hofoingi',
+        'urda','golka','rokpol','telemea','bryndza','parenica','kackavalj','liptauer','greve','korbaciky','herrgardsost',
+        'vasterbottensost','mish','anejo','quesillo','nacho','reggianito','catupiry','queso_paipa','canastra','port_wine',
+        'cados','brie_de_meaux'
     ];
 
     let type = (global.stats.hasOwnProperty('reset') ? global.stats.reset : 0) % cheeses.length;
