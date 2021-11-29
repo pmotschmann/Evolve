05ee1a4083454374ea7a59b4734353f1ef4358bf
Peter Motschmann <pmotsch3@gmail.com>
2021-11-28 21:12:38 -0500

    inc beta flag

--- src/vars.js
+++ src/vars.js
@@ -918,7 +918,7 @@ if (convertVersion(global['version']) < 102000){
 
 global['version'] = '1.2.0';
 delete global['revision'];
-global['beta'] = 14;
+global['beta'] = 15;
 
 if (!global.hasOwnProperty('power')){
     global['power'] = [];       
