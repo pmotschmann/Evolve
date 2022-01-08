b3d42efa075f3ba33ec572a5e0870345f5c89f4f
Peter Motschmann <pmotsch3@gmail.com>
2022-01-08 11:58:26 -0500

    Merged zig patch Resolves #539

--- src/vars.js
+++ src/vars.js
@@ -969,7 +969,7 @@ if (convertVersion(global['version']) < 102007){
 
 global['version'] = '1.2.7';
 delete global['revision'];
-global['beta'] = 2;
+global['beta'] = 3;
 
 if (!global.hasOwnProperty('power')){
     global['power'] = [];       
