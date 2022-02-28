db942fd42913a3983c17f87fbae1716648e70bec
Peter Motschmann <pmotsch3@gmail.com>
2022-02-28 07:35:59 -0500

    Prevent old saves from discovering vault if already opened

--- src/vars.js
+++ src/vars.js
@@ -971,11 +971,14 @@ if (convertVersion(global['version']) < 102012){
     if (global.city.hasOwnProperty('ptrait')){
         global.city.ptrait = [global.city.ptrait];
     }
+    if (global.tech['hell_ruins'] && global.tech.hell_ruins >= 3){
+        global.tech['hell_vault'] = 1;
+    }
 }
 
 global['version'] = '1.2.12';
 delete global['revision'];
-global['beta'] = 2;
+delete global['beta'];
 
 if (!global.hasOwnProperty('power')){
     global['power'] = [];       
