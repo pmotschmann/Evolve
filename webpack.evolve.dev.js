eef637105ef1fd7fa6aca0f8619324ca65ee0ab1
John Haugeland <stonecypher@gmail.com>
2023-10-31 15:25:02 -0700

    Silences spurious file and entrypoint size warnings.  Fixes pmotschmann/Evolve#981

--- webpack.evolve.dev.js
+++ webpack.evolve.dev.js
@@ -3,8 +3,12 @@ var path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/main.js',
+    performance: {
+        maxAssetSize: 2_500_000,       // silence half of the 244k webpack warnings
+        maxEntrypointSize: 2_500_000   // and the other half
+    },
     output: {
-    path: path.resolve(__dirname, 'evolve'),
+        path: path.resolve(__dirname, 'evolve'),
         filename: 'main.js'
     }
 };
--- webpack.wiki.prod.js
+++ webpack.wiki.prod.js
@@ -3,8 +3,12 @@ var path = require('path');
 module.exports = {
     mode: 'production',
     entry: './src/wiki/wiki.js',
+    performance: {
+        maxAssetSize: 2_500_000,       // silence half of the 244k webpack warnings
+        maxEntrypointSize: 2_500_000   // and the other half
+    },
     output: {
-    path: path.resolve(__dirname, 'wiki'),
+        path: path.resolve(__dirname, 'wiki'),
         filename: 'wiki.js'
     }
 };
