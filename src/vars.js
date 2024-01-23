be7d085edd9515484e2989133e93de9ff3139818
Peter Motschmann <pmotsch3@gmail.com>
2024-01-23 08:11:43 -0500

    Beta 5 Build

--- src/vars.js
+++ src/vars.js
@@ -1170,7 +1170,7 @@ if (convertVersion(global['version']) < 103002){
 
 global['version'] = '1.3.10';
 delete global['revision'];
-global['beta'] = 4;
+global['beta'] = 5;
 
 if (!global.hasOwnProperty('prestige')){
     global.prestige = {};
