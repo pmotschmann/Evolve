1fb4f315b9f85f1db1d3af634e4fccd7c5124382
Peter Motschmann <pmotsch3@gmail.com>
2024-01-23 17:21:57 -0500

    Release version

--- src/vars.js
+++ src/vars.js
@@ -1170,7 +1170,7 @@ if (convertVersion(global['version']) < 103002){
 
 global['version'] = '1.3.10';
 delete global['revision'];
-global['beta'] = 5;
+delete global['beta'];
 
 if (!global.hasOwnProperty('prestige')){
     global.prestige = {};
