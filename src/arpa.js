c797368c26d2277cc7c5dce80fbe8284b0482151
CondoSlime <Pimhazeveld@live.nl>
2024-02-07 20:01:27 +0100

    Squashed commit of the following:
    commit 2cbc7a4440edbef2a5efac811eea5f023a0f1fee
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:50:20 2024 +0100
    
        Update main.js
    
    commit 14ab6c608c241d54cd873f79e88e7aef8eaeccc1
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:49:00 2024 +0100
    
        Update main.js
    
    commit 7694c4300b8f4d9192793531f62c9348e5ca3c9f
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:48:04 2024 +0100
    
        Update main.js
    
    commit 2bec1f1a107db5e01cf991d768e3e92a71df9d3e
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:45:58 2024 +0100
    
        Update main.js
    
    commit f7c44b9e8cb123a36e15de11e8e4f3a6d3fed5e9
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:45:46 2024 +0100
    
        Update jobs.js
    
    commit 569b3ece3690ad5685606fcbcd1fcf1b2dddf35c
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:33:08 2024 +0100
    
        Update arpa.js
    
    commit 1a385401bec8f95b40576dd741ad8b2f10ea7414
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:32:46 2024 +0100
    
        Update jobs.js
    
    commit d20be84b15f22fbe625734f2595bdac0d7182c6e
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Wed Feb 7 00:32:34 2024 +0100
    
        Update main.js
    
    commit 7fe1e55b012c5171f11c0231fb324bc4e0e566bc
    Author: Mipha <Pimhazeveld@live.nl>
    Date:   Tue Feb 6 23:44:58 2024 +0100
    
        Update arpa.js

--- src/arpa.js
+++ src/arpa.js
@@ -1609,13 +1609,15 @@ export function arpaAdjustCosts(costs,offset,wiki){
 }
 
 function creativeAdjust(costs,offset,wiki){
-    if ((wiki && wiki.creative) || (!wiki && global.race['creative'])){
+    let fathom = fathomCheck('human');
+    if ((wiki && wiki.creative) || (!wiki && global.race['creative']) || (!wiki && fathom > 0)){
         var newCosts = {};
-        let fathom = fathomCheck('human');
         Object.keys(costs).forEach(function (res){
             newCosts[res] = function(){
                 let cost = costs[res](offset, wiki);
-                cost *= (1 - traits.creative.vars()[1] / 100);
+                if((wiki && wiki.creative) || (!wiki && global.race['creative'])){
+                    cost *= (1 - traits.creative.vars()[1] / 100);
+                }
                 if (fathom > 0){
                     cost *= 1 - (traits.creative.vars(1)[1] / 100 * fathom);
                 }
@@ -1627,15 +1629,19 @@ function creativeAdjust(costs,offset,wiki){
     return costs;
 }
 
-function costMultiplier(project,offset,base,mutiplier,wiki){
+function costMultiplier(project,offset,base,multiplier,wiki){
     var rank = global.arpa[project] ? global.arpa[project].rank : 0;
     if (((wiki && wiki.creative) || (!wiki && global.race['creative'])) && project !== 'syphon'){
-        mutiplier -= traits.creative.vars()[0];
+        multiplier -= traits.creative.vars()[0];
+    }
+    let fathom = fathomCheck('human');
+    if(fathom > 0){
+        multiplier -= traits.creative.vars(1)[0];
     }
     if (offset){
         rank += offset;
     }
-    return Math.round((mutiplier ** rank) * base);
+    return Math.round((multiplier ** rank) * base);
 }
 
 function physics(){
