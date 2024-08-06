57bcb596f674458a0df2c66df20a1e18a68d1ddf
yarukishi <150128243+yarukishi@users.noreply.github.com>
2024-08-06 21:29:29 +0900

    Fix typo in changelog

--- src/wiki/change.js
+++ src/wiki/change.js
@@ -5,15 +5,7 @@ export const changeList = [
     {
         version: `1.3.13`,
         revision: `a`,
-        date: `8/6/2023`,
-        changes: [
-            `Fix a bug that prevented crafting in Cataclysm and Lone Survivor modes.`
-        ]
-    },
-    {
-        version: `1.3.13`,
-        revision: `a`,
-        date: `8/6/2023`,
+        date: `8/6/2024`,
         changes: [
             `Fix a bug that prevented crafting in Cataclysm and Lone Survivor modes.`
         ]
