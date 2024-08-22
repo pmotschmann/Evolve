524b57fe0edf7dd5d61729d611768ed01a59f405
Tom <49035039+tomfrost435@users.noreply.github.com>
2024-08-22 10:22:44 +0200

    Update convert zh-CN to zh-TW using opencc.py

--- strings/convert zh-CN to zh-TW using opencc.py
+++ strings/convert zh-CN to zh-TW using opencc.py
@@ -2,17 +2,24 @@
 from opencc import OpenCC
 
 
-def main(input_file: str, output_file: str) -> None:
-    with open(input_file, "r", encoding="utf-8") as f:
-        zh_CN: str = f.read()
+def main() -> None:
+    """
+    Reads the content of a Simplified Chinese JSON file, converts it to Traditional Chinese,
+    and writes the converted content to a new JSON file.
 
-    # convert zh_CN to zh_TW
-    opencc: OpenCC = OpenCC("s2tw")
-    zh_TW: str = opencc.convert(zh_CN)
+    This script reads the content of the "strings.zh-CN.json" file, converts the content from
+    Simplified Chinese (zh-CN) to Traditional Chinese (zh-TW) using OpenCC, and writes the
+    converted content to the "strings.zh-TW_autotranslated.json" file.
+    """
+    # Read the content of the file
+    with open("strings.zh-CN.json", "r", encoding="utf-8") as f:
+        zh_CN = f.read()
 
-    with open(output_file, "w", encoding="utf-8") as f:
+    # Convert zh_CN to zh_TW and write to a new file
+    with open("strings.zh-TW_autotranslated.json", "w", encoding="utf-8") as f:
+        zh_TW = OpenCC("s2tw").convert(zh_CN)
         f.write(zh_TW)
 
 
 if __name__ == "__main__":
-    main("strings.zh-CN.json", "strings.zh-TW_autotranslated.json")
+    main()
