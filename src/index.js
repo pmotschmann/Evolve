9a7dd9b6cfecfe206b4f9f4d3e86c8c686f240e4
yarukishi <150128243+yarukishi@users.noreply.github.com>
2024-09-02 07:45:04 +0900

    Accept both .txt and .json extensions by default for string pack import (#1166)
    * Accept both .txt and .json for string packs
    
    * Update src/index.js
    
    Co-authored-by: Dan Wallis <dan@wallis.nz>
    
    ---------
    
    Co-authored-by: Dan Wallis <dan@wallis.nz>

--- src/index.js
+++ src/index.js
@@ -1380,7 +1380,7 @@ export function index(){
         </div>
         <div class="stringPack setting">
             <button id="stringPack" class="button" @click="importStringFile">{{ 'load_string_pack' | label }}</button>
-            <input type="file" class="fileImport" id="stringPackFile" accept=".txt">
+            <input type="file" class="fileImport" id="stringPackFile" accept="text/plain, application/json">
             <button class="button right" @click="clearStringFile">{{ 'clear_string_pack' | label }}</button>
         </div>
         <div class="stringPack setting">
