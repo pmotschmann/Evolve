0342a8dc66cc9411597517ff17e777e98ed495e1
yarukishi <150128243+yarukishi@users.noreply.github.com>
2024-09-02 12:33:27 +0900

    Remove manual crafting cost discount from High Pop (#1165)
    * Remove manual crafting cost discount from High Pop
    
    * Provide only manual crafting costs to scripts
    
    * Also update the other line

--- src/debug.js
+++ src/debug.js
@@ -17,7 +17,7 @@ export function enableDebug(){
             races: deepClone(races),
             traits: deepClone(traits),
             tradeRatio: deepClone(tradeRatio),
-            craftCost: deepClone(craftCost()),
+            craftCost: deepClone(craftCost(true)),
             atomic_mass: deepClone(atomic_mass),
             f_rate: deepClone(f_rate),
             checkAffordable: deepClone(checkAffordable),
@@ -41,7 +41,7 @@ export function enableDebug(){
 export function updateDebugData(){
     if (global.settings.expose){
         window.evolve.global = deepClone(global);
-        window.evolve.craftCost = deepClone(craftCost()),
+        window.evolve.craftCost = deepClone(craftCost(true)),
         window.evolve.breakdown = deepClone(breakdown);
     }
 }
