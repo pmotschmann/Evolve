fc80741da8162052bd88fd02b95753a12f1083f5
Mipha <Pimhazeveld@live.nl>
2024-09-02 21:18:44 +0200

    Added fix for factory production tooltip. (#1187)
    Factory lux goods tooltip accidentally showed money production with global production modifiers applied twice.

--- src/main.js
+++ src/main.js
@@ -4097,14 +4097,13 @@ function fastLoop(){
 
                 let delta = workDone * demand;
                 if (global.race['gravity_well']){ delta = teamster(delta); }
-                delta *= global_multiplier * hunger;
+                FactoryMoney = delta;
 
                 if (global.race['discharge'] && global.race['discharge'] > 0){
                     delta *= 0.5;
                 }
 
-                FactoryMoney = delta;
-                modRes('Money', delta * time_multiplier);
+                modRes('Money', delta * hunger * global_multiplier * time_multiplier);
             }
 
             if (global.city.factory['Furs'] && global.city.factory['Furs'] > 0){
@@ -6941,7 +6940,8 @@ function fastLoop(){
         breakdown.p['Iridium'][loc('hunger')] = ((hunger - 1) * 100) + '%';
 
         // Income
-        let rawCash = FactoryMoney || 0;
+        let rawCash = FactoryMoney ? FactoryMoney * global_multiplier * hunger : 0;
+        if (FactoryMoney && global.race['discharge'] && global.race['discharge'] > 0){rawCash *= 0.5;}
         if (global.tech['currency'] >= 1){
             let income_base = global.resource[global.race.species].amount + global.civic.garrison.workers - global.civic.unemployed.workers;
             if (global.race['high_pop']){
