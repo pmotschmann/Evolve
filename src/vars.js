7c407655ad8ea1a6a891bd9b023a142070fa0adb
Peter Motschmann <pmotsch3@gmail.com>
2024-10-12 16:32:38 -0400

    Fixed some CSS issues

--- src/vars.js
+++ src/vars.js
@@ -1206,7 +1206,7 @@ if (convertVersion(global['version']) <= 103015){
 
 global['version'] = '1.3.17';
 delete global['revision'];
-global['beta'] = 1;
+global['beta'] = 2;
 
 if (!global.hasOwnProperty('prestige')){
     global.prestige = {};
