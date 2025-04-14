pmotschmann/Evolve

commit 8f40f884a57da83ce333f936d6898d4e636cbff2

Add more structs to alwaysPower setting (#1384)
* Add more structs to alwaysPower setting

Centralize power_reqs checks to `powerOnNewStruct()`.

Allow mills to be automatically powered on after researching Wind Turbine.

Automatically power on mines, coal mines, rock quarries, cement plants, and metal refineries when the alwaysPower flag is set. Wardenclyffes and sawmills already did this, so no change to those. In all cases, the setting takes effect when the building is constructed, not when the technology is researched.

Only when the alwaysPower flag is enabled, automatically power on the completed World Collider, Stargate, Ascension Machine, Devilish Dish, or Waygate.

Force the Guard Post and Cultural Center to respect the usual rules for powering on. If the alwaysPower switch is disabled and power is insufficient, then no longer automatically power them on at time of completion.

Add initStruct-compatible definitions to some more structs.

* Actually check alwaysPower for ascension machine

src/actions.js
@@ -1975,6 +1975,10 @@ export const actions = {
             action(){
                 if (payCosts($(this)[0])){
                     incrementStruct('mill','city');
+                    // Prevent alwaysPower from enabling mills that were built before researching Wind Turbines
+                    if (checkPowerRequirements($(this)[0]){
+                        powerOnNewStruct($(this)[0]);
+                    }
                     return true;
                 }
                 return false;
@@ -2746,9 +2750,7 @@ export const actions = {
                         global.settings.showIndustry = true;
                         global['resource']['Chrysotile'].max += stone;
                     }
-                    if (global.tech['mine_conveyor']){
-                        powerOnNewStruct($(this)[0]);
-                    }
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -2799,9 +2801,7 @@ export const actions = {
                     incrementStruct('cement_plant','city');
                     global.civic.cement_worker.display = true;
                     global.civic.cement_worker.max = global.city.cement_plant.count * jobScale(2);
-                    if (global.tech['cement'] && global.tech['cement'] >= 5){
-                        powerOnNewStruct($(this)[0]);
-                    }
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -3040,9 +3040,7 @@ export const actions = {
                         global.resource.Sheet_Metal.display = true;
                         loadFoundry();
                     }
-                    if (global.tech['alumina'] >= 2){
-                        powerOnNewStruct($(this)[0]);
-                    }
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -3089,9 +3087,7 @@ export const actions = {
                     global.resource.Copper.display = true;
                     global.civic.miner.display = true;
                     global.civic.miner.max = jobScale(global.city.mine.count);
-                    if (global.tech['mine_conveyor']){
-                        powerOnNewStruct($(this)[0]);
-                    }
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -3140,9 +3136,7 @@ export const actions = {
                     global.resource.Coal.display = true;
                     global.civic.coal_miner.display = true;
                     global.civic.coal_miner.max = jobScale(global.city.coal_mine.count);
-                    if (global.tech['mine_conveyor']){
-                        powerOnNewStruct($(this)[0]);
-                    }
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -6839,7 +6833,7 @@ export function powerOnNewStruct(c_action,extra){
     let need_p = c_action.hasOwnProperty('powered') && c_action.powered() > 0;
     let can_p = !need_p;
     let gov_replicator = global.race.hasOwnProperty('governor') && global.race.governor.hasOwnProperty('tasks') && global.race.hasOwnProperty('replicator') && Object.values(global.race.governor.tasks).includes('replicate') && global.race.governor.config.replicate.pow.on && global.race.replicator.pow > 0;
-    if (need_p && global.city.hasOwnProperty('powered')){
+    if (need_p && global.city.hasOwnProperty('powered') && checkPowerRequirements(c_action)){
         let power = global.city.power;
         if (gov_replicator){
             power += global.race.replicator.pow;

src/space.js
@@ -2470,7 +2470,12 @@ const spaceProjects = {
                     incrementStruct('world_collider');
                     if (global.space.world_collider.count >= 1859){
                         global.tech['science'] = 11;
-                        global.space['world_controller'] = { count: 1, on: 0 };
+                        initStruct(spaceProjects.spc_dwarf.world_controller);
+                        incrementStruct('world_controller');
+                        // Require the force power-on setting to automatically power end-of-era structs, even when power is abundant
+                        if (global.settings.alwaysPower){
+                            powerOnNewStruct(spaceProjects.spc_dwarf.world_controller);
+                        }
                         drawTech();
                         renderSpace();
                         if (global.race['banana']){
@@ -2626,7 +2631,8 @@ const spaceProjects = {
                     global.space.mass_relay.count++;
                     if (global.space.mass_relay.count >= 100){
                         global.tech['outer'] = 6;
-                        global.space['m_relay'] = { count: 1, on: 1, charged: 0 };
+                        initStruct(spaceProjects.spc_dwarf.m_relay);
+                        powerOnNewStruct(spaceProjects.spc_dwarf.m_relay);
                         drawTech();
                         renderSpace();
                         clearPopper();
@@ -2665,6 +2671,12 @@ const spaceProjects = {
             },
             action(){
                 return false;
+            },
+            struct(){
+                return {
+                    d: { count: 0, on: 0, charged: 0 },
+                    p: ['m_relay','space']
+                };
             }
         },
     },
@@ -4334,8 +4346,12 @@ const interstellarProjects = {
                         incrementStruct('stargate','interstellar');
                         if (global.interstellar.stargate.count >= 200){
                             global.tech['stargate'] = 4;
-                            global.interstellar['s_gate'] = { count: 1, on: 0 };
-                            powerOnNewStruct($(interstellarProjects.int_blackhole.s_gate)[0]);
+                            initStruct(interstellarProjects.int_blackhole.s_gate);
+                            incrementStruct('s_gate','interstellar');
+                            // Require the force power-on setting to automatically power end-of-era structs, even when power is abundant
+                            if (global.settings.alwaysPower){
+                                powerOnNewStruct(interstellarProjects.int_blackhole.s_gate);
+                            }
                             deepSpace();
                             clearPopper();
                         }
@@ -4568,7 +4584,10 @@ const interstellarProjects = {
                         incrementStruct('ascension_machine','interstellar');
                         if (global.interstellar.ascension_machine.count >= 100){
                             global.tech['ascension'] = 7;
-                            global.interstellar['ascension_trigger'] = { count: 1, on: 0 };
+                            initStruct(interstellarProjects.int_sirius.ascension_trigger);
+                            if (global.settings.alwaysPower){
+                                powerOnNewStruct(interstellarProjects.int_sirius.ascension_trigger);
+                            }
                             deepSpace();
                             clearPopper();
                         }
@@ -4658,6 +4677,12 @@ const interstellarProjects = {
             },
             action(){
                 return false;
+            },
+            struct(){
+                return {
+                    d: { count: 0, on: 0 },
+                    p: ['ascension_trigger','interstellar']
+                };
             }
         },
         ascend: {

src/portal.js
@@ -1555,7 +1555,7 @@ const fortressModules = {
             action(){
                 if (payCosts($(this)[0])){
                     incrementStruct('guard_post','portal');
-                    global.portal.guard_post.on++;
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;
@@ -2480,8 +2480,11 @@ const fortressModules = {
                     global.portal['oven'].count++;
                     if (global.portal.oven.count >= 100){
                         global.tech['dish'] = 3;
-                        global.portal['oven_complete'] = { count: 1, on: 0 };
-                        global.portal['devilish_dish'] = { count: 0, done: 0, time: 0 };
+                        initStruct(fortressModules.prtl_lake.oven_complete);
+                        if (global.settings.alwaysPower){
+                            powerOnNewStruct(fortressModules.prtl_lake.oven_complete);
+                        }
+                        initStruct(fortressModules.prtl_lake.devilish_dish);
                         renderFortress();
                         clearPopper();
                     }
@@ -2517,6 +2520,12 @@ const fortressModules = {
             p_fuel(){ return { r: 'Infernite', a: 225 }},
             action(){
                 return false;
+            },
+            struct(){
+                return {
+                    d: { count: 0, on: 0 },
+                    p: ['oven_complete','portal']
+                };
             }
         },
         devilish_dish: {
@@ -2532,6 +2541,12 @@ const fortressModules = {
             },
             action(){
                 return false;
+            },
+            struct(){
+                return {
+                    d: { count: 0, done: 0, time: 0 },
+                    p: ['devilish_dish','portal']
+                };
             }
         },
         dish_soul_steeper: {
@@ -3092,6 +3107,9 @@ const fortressModules = {
                     if (global.portal.waygate.count >= 10){
                         global.tech.waygate = 2;
                         global.portal.waygate.count = 1;
+                        if (global.settings.alwaysPower){
+                            global.portal.waygate.on = 1;
+                        }
                         renderFortress();
                         drawTech();
                     }

src/truepath.js
@@ -582,8 +582,8 @@ const outerTruth = {
                         incrementStruct('ai_core');
                         if (global.space.ai_core.count >= 100){
                             global.tech['titan_ai_core'] = 1;
-                            global.space['ai_core2'] = { count: 1, on: 0 };
-                            powerOnNewStruct($(outerTruth.spc_titan.ai_core2)[0]);
+                            initStruct(outerTruth.spc_titan.ai_core2);
+                            powerOnNewStruct(outerTruth.spc_titan.ai_core2);
                             renderSpace();
                             drawTech();
                             if (global.city.ptrait.includes('kamikaze') && !global.race['tidal_decay']){
@@ -639,6 +639,12 @@ const outerTruth = {
             },
             flair(){
                 return global.space.hasOwnProperty('ai_core2') && global.space.ai_core2.on >= 1 ? loc(`space_ai_core_flair`) : loc(`space_ai_core_flair2`);
+            },
+            struct(){
+                return {
+                    d: { count: 0, on: 0 },
+                    p: ['ai_core2','space']
+                };
             }
         },
         ai_colonist: {
@@ -2641,7 +2647,7 @@ const tauCetiModules = {
             action(){
                 if (payCosts($(this)[0])){
                     incrementStruct('tau_cultural_center','tauceti');
-                    global.tauceti.tau_cultural_center.on++;
+                    powerOnNewStruct($(this)[0]);
                     return true;
                 }
                 return false;

commit 26ddb2e09cb721af14ff88742fba02fe2d39c6a0

Fix mistakes introduced in #1384
Increment the ascension trigger count to 1 after it is initialized.
Check power requirements before supplying power to structures.

src/actions.js
@@ -6888,7 +6888,7 @@ export function getStructNumActive(c_action,wiki){
 
     // Electricity: production is negative, consumption is positive
     if (c_action.hasOwnProperty('powered') && c_action.powered() > 0) {
-        if (global.city.hasOwnProperty('powered')){
+        if (global.city.hasOwnProperty('powered') && checkPowerRequirements(c_action)){
             // The p_on struct is empty in the wiki view
             if (!wiki){
                 num_on = p_on[parts[1]];

src/main.js
@@ -7,7 +7,7 @@ import { defineResources, resource_values, spatialReasoning, craftCost, plasmidB
 import { defineJobs, job_desc, loadFoundry, farmerValue, jobName, jobScale, workerScale, limitCraftsmen, loadServants} from './jobs.js';
 import { defineIndustry, f_rate, manaCost, setPowerGrid, gridEnabled, gridDefs, nf_resources, replicator, luxGoodPrice, smelterUnlocked } from './industry.js';
 import { checkControlling, garrisonSize, armyRating, govTitle, govCivics, govEffect, weaponTechModifer } from './civics.js';
-import { actions, updateDesc, checkTechRequirements, drawEvolution, BHStorageMulti, storageMultipler, checkAffordable, drawCity, drawTech, gainTech, housingLabel, updateQueueNames, wardenLabel, planetGeology, resQueue, bank_vault, start_cataclysm, orbitDecayed, postBuild, skipRequirement, structName, templeCount, initStruct, casino_vault, casinoEarn } from './actions.js';
+import { actions, updateDesc, checkTechRequirements, drawEvolution, BHStorageMulti, storageMultipler, checkAffordable, checkPowerRequirements, drawCity, drawTech, gainTech, housingLabel, updateQueueNames, wardenLabel, planetGeology, resQueue, bank_vault, start_cataclysm, orbitDecayed, postBuild, skipRequirement, structName, templeCount, initStruct, casino_vault, casinoEarn } from './actions.js';
 import { renderSpace, convertSpaceSector, fuel_adjust, int_fuel_adjust, zigguratBonus, planetName, genPlanets, setUniverse, universe_types, gatewayStorage, piracy, spaceTech, universe_affixes } from './space.js';
 import { renderFortress, bloodwar, soulForgeSoldiers, hellSupression, genSpireFloor, mechRating, mechCollect, updateMechbay, hellguard } from './portal.js';
 import { asphodelResist, mechStationEffect, renderEdenic } from './edenic.js';
@@ -2003,7 +2003,7 @@ function fastLoop(){
             const region = parts[0] === 'city' ? parts[0] : convertSpaceSector(parts[0]);
             const c_action = parts[0] === 'city' ? actions.city[struct] : actions[region][parts[0]][struct];
             if (global[region][struct]?.on){
-                if (region !== 'galaxy' || p_on['s_gate']){
+                if (checkPowerRequirements(c_action) && (region !== 'galaxy' || p_on['s_gate'])){
                     totalPowerDemand += global[region][struct].on * c_action.powered();
                     p_on[struct] = global[region][struct].on;
                 } else {

src/space.js
@@ -4599,6 +4599,7 @@ const interstellarProjects = {
                         if (global.interstellar.ascension_machine.count >= 100){
                             global.tech['ascension'] = 7;
                             initStruct(interstellarProjects.int_sirius.ascension_trigger);
+                            incrementStruct('ascension_trigger','interstellar');
                             if (global.settings.alwaysPower){
                                 powerOnNewStruct(interstellarProjects.int_sirius.ascension_trigger);
                             }