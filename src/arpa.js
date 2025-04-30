3e9cc61818bfd15697665eb2af1a4dd8ed289d72
AERobert <egglestonarobert@gmail.com>
2025-04-30 13:49:55 -0400

    add ARIA headings for each project
    The Projects tab lacked headings for each project, which made it more annoying to navigate with a screen reader. This update modifies the spans containing each project title (which were previously hidden from screen readers), removes aria-hidden="true" so they can be detected, and assigns role="heading" with aria-level="3" to make them appear as level 3 headings to screen readers.

--- src/arpa.js
+++ src/arpa.js
@@ -2430,7 +2430,7 @@ function addProject(parent,project){
         parent.append(current);
 
         let title = typeof arpaProjects[project].title === 'string' ? arpaProjects[project].title : arpaProjects[project].title();
-        let head = $(`<div class="head"><span aria-hidden="true" class="desc has-text-warning">${title}</span><a v-on:click="srDescAndEffect" class="is-sr-only" role="button">${title} description</a><span aria-hidden="true" v-show="rank" class="rank">{{ rank | level }}</span><span class="is-sr-only">{{ rank | level }}</span></div>`);
+        let head = $(`<div class="head"><span class="desc has-text-warning" role="heading" aria-level="3">${title}</span><a v-on:click="srDescAndEffect" class="is-sr-only" role="button">${title} description</a><span aria-hidden="true" v-show="rank" class="rank">{{ rank | level }}</span><span class="is-sr-only">{{ rank | level }}</span></div>`);
         current.append(head);
 
         let progress = $(`<div class="pbar"><progress class="progress" :value="complete" max="100"></progress><span class="progress-value has-text-danger">{{ complete }}%</span></div>`);
