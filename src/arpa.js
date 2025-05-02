f1892d078705acdaace86b5e20333e9303a6bc0f
AERobert <67764127+AERobert@users.noreply.github.com>
2025-05-01 22:54:10 -0400

    ARPA accessibility improvements (#1432)
    * use correct project name for queue labels
    
    The buttons to add an ARPA project to the building queue used the internal identifiers instead of the projects name. This would mean the label would say something like "Queue lhc" instead of "Queue Supercollider". This changes the label to use the "title" variable instead of"project", which contains the correct and localized name.""
    
    * combine project descriptions into one button
    
    Previously, there were two separate clickable elements for each ARPA project to separately speak the project's description and effect strings: specifically, the element with the name of the project and the one showing the current level of the project (both screen reader only). I thought this was confusing, so I combined the "srDesc" and "srEffect" methods into one to speak both, and I changed the first element to use the combined method, added role="button", and included "description" in the label so it appears similar to the other description buttons in the game. The element showing the project level was changed to be non-interactive when clicked and was converted from an anchor to a span element so screen readers do not announce it as a link or "clickable".
    
    * add ARIA headings for each project
    
    The Projects tab lacked headings for each project, which made it more annoying to navigate with a screen reader. This update modifies the spans containing each project title (which were previously hidden from screen readers), removes aria-hidden="true" so they can be detected, and assigns role="heading" with aria-level="3" to make them appear as level 3 headings to screen readers.
    
    * add headings to Genetics tab
    
    The Genetics tab also lacked navigational headings. This change adds ARIA roles to make the section labels for species' minor traits, major traits, and available traits (when unlocked) into heading level 3s. Screen-reader-only h4 elements were also added before each minor trait using the "is-sr-only" class. The traits in the other two sections are not navigated between often enough to require subheadings.

--- src/arpa.js
+++ src/arpa.js
@@ -1964,7 +1964,7 @@ function genetics(){
             }
         });
 
-        breakdown.append(`<div class="trait major has-text-success">${loc('arpa_race_genetic_traids',[flib('name')])}</div>`)
+        breakdown.append(`<div class="trait major has-text-success" role="heading" aria-level="3">${loc('arpa_race_genetic_traids',[flib('name')])}</div>`)
 
         let traitName = traitSkin('name');
 
@@ -2019,7 +2019,7 @@ function genetics(){
         let trait_list = [];
         if (global.genes['mutation'] && global.genes['mutation'] >= 3){
             if (global.race.species !== 'hellspawn' && ((global.race.species !== 'sludge' && global.race.species !== 'ultra_sludge') || !global.race['modified'])){
-                breakdown.append(`<div class="trait major has-text-success">${loc('arpa_race_genetic_gain')}</div>`);
+                breakdown.append(`<div class="trait major has-text-success" role="heading" aria-level="3">${loc('arpa_race_genetic_gain')}</div>`);
 
                 let conflict_traits = ['dumb','smart']; //Conflicting traits are paired together
                 let mainType = races[global.race.species].type === 'hybrid' ? global.race.maintype : races[global.race.species].type
@@ -2079,7 +2079,7 @@ function genetics(){
         }
 
         if (minor){
-            breakdown.prepend(`<div class="trait minor has-text-success">${loc('arpa_race_genetic_minor_traits',[flib('name')])}</div>`)
+            breakdown.prepend(`<div class="trait minor has-text-success" role="heading" aria-level="3">${loc('arpa_race_genetic_minor_traits',[flib('name')])}</div>`)
         }
 
         let rmCost = function(t){
@@ -2384,7 +2384,7 @@ export function sequenceLabs(){
 
 function bindTrait(breakdown,trait){
     let m_trait = $(`<div class="trait t-${trait} traitRow"></div>`);
-    let gene = $(`<span v-bind:class="['basic-button', 'gene', 'gbuy', genePurchasable('${trait}') ? '' : 'has-text-fade']" role="button" :aria-label="geneCost('${trait}')" @click="gene('${trait}')">${global.resource.Genes.name} (${global.race.minor[trait] || 0})</span>`);
+    let gene = $(`<h4 class="is-sr-only">${trait}</h4><span v-bind:class="['basic-button', 'gene', 'gbuy', genePurchasable('${trait}') ? '' : 'has-text-fade']" role="button" :aria-label="geneCost('${trait}')" @click="gene('${trait}')">${global.resource.Genes.name} (${global.race.minor[trait] || 0})</span>`);
     m_trait.append(gene);
     if (global.prestige.Phage.count > 0){
         let phage = $(`<span v-bind:class="['basic-button', 'gene', 'pbuy', phagePurchasable('${trait}') ? '' : 'has-text-fade']" role="button" :aria-label="phageCost('${trait}')" @click="phage('${trait}')">${loc('resource_Phage_name')} (${global.genes.minor[trait] || 0})</span>`);
@@ -2430,7 +2430,7 @@ function addProject(parent,project){
         parent.append(current);
 
         let title = typeof arpaProjects[project].title === 'string' ? arpaProjects[project].title : arpaProjects[project].title();
-        let head = $(`<div class="head"><span aria-hidden="true" class="desc has-text-warning">${title}</span><a v-on:click="srDesc" class="is-sr-only">${title}</a><span aria-hidden="true" v-show="rank" class="rank">{{ rank | level }}</span><a v-on:click="srLevel" class="is-sr-only">{{ rank | level }}</a></div>`);
+        let head = $(`<div class="head"><span class="desc has-text-warning" role="heading" aria-level="3">${title}</span><a v-on:click="srDescAndEffect" class="is-sr-only" role="button">${title} description</a><span aria-hidden="true" v-show="rank" class="rank">{{ rank | level }}</span><span class="is-sr-only">{{ rank | level }}</span></div>`);
         current.append(head);
 
         let progress = $(`<div class="pbar"><progress class="progress" :value="complete" max="100"></progress><span class="progress-value has-text-danger">{{ complete }}%</span></div>`);
@@ -2439,7 +2439,7 @@ function addProject(parent,project){
         let buy = $('<div class="buy"></div>');
         current.append(buy);
 
-        buy.append($(`<button aria-label="${loc('queue')} ${project}" class="button" @click="queue('${project}')">${loc('queue')}</button>`));
+        buy.append($(`<button aria-label="${loc('queue')} ${title}" class="button" @click="queue('${project}')">${loc('queue')}</button>`));
         buy.append($(`<button :aria-label="arpaProjectSRCosts('1','${project}')" class="button x1" @click="build('${project}',1)">1%</button>`));
         buy.append($(`<button :aria-label="arpaProjectSRCosts('10','${project}')" class="button x10" @click="build('${project}',10)">10%</button>`));
         buy.append($(`<button :aria-label="arpaProjectSRCosts('25','${project}')" class="button x25" @click="build('${project}',25)">25%</button>`));
@@ -2481,11 +2481,10 @@ function addProject(parent,project){
                 build(pro,num){
                     buildArpa(pro,num,true);
                 },
-                srDesc(){
-                    return srSpeak(typeof arpaProjects[project].desc === 'string' ? arpaProjects[project].desc : arpaProjects[project].desc());
-                },
-                srLevel(){
-                    return srSpeak(arpaProjects[project].effect());
+                srDescAndEffect(){
+                    let desc = typeof arpaProjects[project].desc === 'string' ? arpaProjects[project].desc : arpaProjects[project].desc();
+                    let effect = arpaProjects[project].effect();
+                    return srSpeak(`${desc}\n${effect}`);
                 },
                 arpaProjectSRCosts(id,project){
                     let inc = id === '100' ? 100 - global.arpa[project].complete : id;
