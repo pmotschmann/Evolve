ee2ed156d05c3210ae477f5848e8307f096093a4
Peter Motschmann <pmotsch3@gmail.com>
2025-05-04 12:23:48 -0400

    Warlord Release

--- src/vars.js
+++ src/vars.js
@@ -1251,7 +1251,7 @@ if (convertVersion(global['version']) <= 104003){
 
 global['version'] = '1.4.3';
 delete global['revision'];
-global['beta'] = 30;
+delete global['beta'];
 
 if (!global.hasOwnProperty('prestige')){
     global.prestige = {};
