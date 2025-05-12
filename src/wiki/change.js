131d3f46824c6f85ac3eebe4829abeb5441cdf9b
Peter Motschmann <pmotsch3@gmail.com>
2025-05-12 08:04:08 -0400

    1.4.5b

--- src/wiki/change.js
+++ src/wiki/change.js
@@ -4,6 +4,14 @@ import { clearElement } from './../functions.js';
 export const changeList = [
     {
         version: `1.4.5`,
+        revision: `b`,
+        date: `5/12/2025`,
+        changes: [
+            `Fixed issues with loading custom race data into custom lab.`
+        ]
+    },
+    {
+        version: `1.4.5`,
         revision: `a`,
         date: `5/12/2025`,
         changes: [
