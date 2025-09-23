pmotschmann/Evolve

commit 1b6c3c62b085e71d392c269b013c82f4f686d7d2

Better resource buffering (#1468)
* Remove one-offs from resource deltas

The following resource production/consumption should not be part of the per-second delta value, because it's not repeatable.

- Resource discovery events for steel (via trade) or elerium (via asteroid belt mining)
- Killing a citizen with psychic powers
- Crate and container assembly
- Combat loot

* Add temporary resource storage buffer during loop

* Prohibit spending resources into the negatives

* Fix magic ritual buffering (produce then consume)

* Fix implementation of gene assembly buffer

* Better fix for magic ritual buffering

---------

Co-authored-by: Peter Motschmann <pmotsch3@gmail.com>

src/main.js
@@ -1,11 +1,11 @@
-import { global, save, seededRandom, webWorker, intervals, keyMap, atrack, resizeGame, breakdown, sizeApproximation, keyMultiplier, power_generated, p_on, support_on, int_on, gal_on, spire_on, set_qlevel, quantum_level, callback_queue } from './vars.js';
+import { global, save, seededRandom, webWorker, intervals, keyMap, atrack, resizeGame, breakdown, sizeApproximation, keyMultiplier, power_generated, p_on, support_on, int_on, gal_on, spire_on, set_qlevel, quantum_level, callback_queue, active_rituals } from './vars.js';
 import { loc } from './locale.js';
 import { unlockAchieve, checkAchievements, drawAchieve, alevel, universeAffix, challengeIcon, unlockFeat, checkAdept } from './achieve.js';
-import { gameLoop, vBind, popover, clearPopper, flib, tagEvent, timeCheck, arpaTimeCheck, timeFormat, powerModifier, modRes, initMessageQueue, messageQueue, calc_mastery, calcPillar, darkEffect, calcQueueMax, calcRQueueMax, buildQueue, shrineBonusActive, getShrineBonus, eventActive, easterEggBind, trickOrTreatBind, powerGrid, deepClone, addATime, exceededATimeThreshold, loopTimers, calcQuantumLevel, drawPet } from './functions.js';
+import { gameLoop, vBind, popover, clearPopper, flib, tagEvent, timeCheck, arpaTimeCheck, timeFormat, powerModifier, resetResBuffer, modRes, initMessageQueue, messageQueue, calc_mastery, calcPillar, darkEffect, calcQueueMax, calcRQueueMax, buildQueue, shrineBonusActive, getShrineBonus, eventActive, easterEggBind, trickOrTreatBind, powerGrid, deepClone, addATime, exceededATimeThreshold, loopTimers, calcQuantumLevel, drawPet } from './functions.js';
 import { races, traits, racialTrait, orbitLength, servantTrait, randomMinorTrait, biomes, planetTraits, shapeShift, fathomCheck, blubberFill, cleanRemoveTrait } from './races.js';
 import { defineResources, resource_values, spatialReasoning, craftCost, plasmidBonus, faithBonus, faithTempleCount, tradeRatio, craftingRatio, crateValue, containerValue, tradeSellPrice, tradeBuyPrice, atomic_mass, supplyValue, galaxyOffers } from './resources.js';
 import { defineJobs, job_desc, loadFoundry, farmerValue, jobName, jobScale, workerScale, limitCraftsmen, loadServants} from './jobs.js';
-import { defineIndustry, f_rate, manaCost, setPowerGrid, gridEnabled, gridDefs, nf_resources, replicator, luxGoodPrice, smelterUnlocked, smelterFuelConfig } from './industry.js';
+import { defineIndustry, f_rate, manaCost, setPowerGrid, gridEnabled, gridDefs, nf_resources, replicator, luxGoodPrice, smelterUnlocked, smelterFuelConfig, setupRituals, maxRitualNum, ritual_types } from './industry.js';
 import { checkControlling, garrisonSize, armyRating, govTitle, govCivics, govEffect, weaponTechModifer } from './civics.js';
 import { actions, updateDesc, checkTechRequirements, drawEvolution, BHStorageMulti, storageMultipler, checkAffordable, checkPowerRequirements, drawCity, drawTech, gainTech, housingLabel, updateQueueNames, wardenLabel, planetGeology, resQueue, bank_vault, start_cataclysm, orbitDecayed, postBuild, skipRequirement, structName, templeCount, initStruct, casino_vault, casinoEarn, doCallbacks, cLabels } from './actions.js';
 import { renderSpace, convertSpaceSector, fuel_adjust, int_fuel_adjust, zigguratBonus, planetName, genPlanets, setUniverse, universe_types, gatewayStorage, piracy, spaceTech, universe_affixes, galaxyRegions, gatewayArmada, galaxy_ship_types } from './space.js';
@@ -287,6 +287,7 @@ buildQueue();
 if (global.race['shapeshifter']){
     shapeShift(false,true);
 }
+setupRituals();
 
 Object.keys(gridDefs()).forEach(function(gridtype){
     powerGrid(gridtype);
@@ -1213,6 +1214,7 @@ function fastLoop(){
     }
 
     var time_multiplier = 0.25;
+    resetResBuffer();
 
     if (global.race.species === 'protoplasm'){
         // Early Evolution Game
@@ -5992,18 +5994,24 @@ function fastLoop(){
         // Mana
         if (global.resource.Mana.display){
             if (global.race['casting']){
-                ['farmer','miner','lumberjack','science','factory','army','hunting','crafting'].forEach(function (spell){
-                    if (global.race.casting[spell] && global.race.casting[spell] > 0){
-                        let consume_mana = manaCost(global.race.casting[spell]);
-                        breakdown.p.consume.Mana[loc(`modal_pylon_spell_${spell}`)] = -(consume_mana);
+                ritual_types.forEach(function (spell){
+                    if (global.race.casting[spell]){
+                        if (global.race.casting[spell] > 0){
+                            const consume_mana = manaCost(global.race.casting[spell]);
+                            const consume_mana_dt = consume_mana * time_multiplier;
+                            if (consume_mana_dt > global.resource.Mana.amount){
+                                active_rituals[spell] = maxRitualNum(global.resource.Mana.amount, time_multiplier);
+                            }
+                            else {
+                                active_rituals[spell] = global.race.casting[spell];
+                            }
+                            breakdown.p.consume.Mana[loc(`modal_pylon_spell_${spell}`)] = -(consume_mana);
 
-                        let buffer = global.resource.Mana.diff > 0 ? global.resource.Mana.diff * time_multiplier : 0
-                        if (!modRes('Mana', -(consume_mana * time_multiplier), false, buffer)){
-                            global.race.casting[spell]--;
+                            modRes('Mana', -(consume_mana_dt));
                         }
-                    }
-                    else {
-                        delete breakdown.p.consume.Mana[loc(`modal_pylon_spell_${spell}`)];
+                        else {
+                            active_rituals[spell] = 0;
+                        }
                     }
                 });
             }
@@ -8075,6 +8083,9 @@ function fastLoop(){
 
     // main resource delta tracking
     Object.keys(global.resource).forEach(function (res) {
+        if (global.resource[res].amount > global.resource[res].max && global.resource[res].max >= 0){
+            global.resource[res].amount = global.resource[res].max;
+        }
         if (global['resource'][res].rate > 0 || (global['resource'][res].rate === 0 && global['resource'][res].max === -1)){
             diffCalc(res,webWorker.mt);
         }
@@ -10875,14 +10886,14 @@ function midLoop(){
             if (Math.rand(0,250) <= belt_mining){
                 global.tech['asteroid'] = 4;
                 global.resource.Elerium.display = true;
-                modRes('Elerium',1);
+                modRes('Elerium',1,true);
                 drawTech();
                 messageQueue(loc('discover_elerium'),'info',false,['progress']);
             }
         }
 
         if (global.tech['asteroid'] && global.tech.asteroid === 4 && global.resource.Elerium.amount === 0){
-            modRes('Elerium',1);
+            modRes('Elerium',1,true);
         }
 
         if (p_on['outpost'] > 0 && global.tech['gas_moon'] && global.tech['gas_moon'] === 1){
@@ -11049,7 +11060,7 @@ function midLoop(){
 
         if (global.race['casting']){
             let total = 0;
-            ['farmer','miner','lumberjack','science','factory','army','hunting','crafting'].forEach(function (spell){
+            ritual_types.forEach(function (spell){
                 if (global.race.casting[spell]){
                     total += global.race.casting[spell];
                 }
@@ -11057,6 +11068,7 @@ function midLoop(){
             global.race.casting.total = total;
         }
 
+        let blockGeneBuffer = false;
         if (global.tech['r_queue'] && global.r_queue.display){
             let idx = -1;
             let c_action = false;
@@ -11089,6 +11101,9 @@ function midLoop(){
                             }
                             else {
                                 if (reqMet){
+                                    if (!stop && t_time <= 1){
+                                        blockGeneBuffer = true;
+                                    }
                                     time += t_time;
                                 }
                                 untime += t_time;
@@ -11148,7 +11163,7 @@ function midLoop(){
         }
 
         if (global.arpa.sequence && global.arpa.sequence['auto'] && global.tech['genetics'] && global.tech['genetics'] >= 8){
-            buildGene();
+            buildGene(blockGeneBuffer);
         }
 
         if (p_on['soul_forge']){
@@ -12838,9 +12853,8 @@ function longLoop(){
     }
 }
 
-function buildGene(){
-    // Reduce size of Knowledge buffer when daily production is under 10000 to avoid jumping in front of the research queue
-    let buffer = global.resource.Knowledge.diff < 10000 ? global.resource.Knowledge.diff : 10000;
+function buildGene(blockGeneBuffer = false){
+    let buffer = blockGeneBuffer ? 0 : 10000;
     if (global.resource.Knowledge.amount >= 200000 && global.resource.Knowledge.amount >= global.resource.Knowledge.max - buffer){
         global.resource.Knowledge.amount -= 200000;
         let gene = global.genes['synthesis'] ? sythMap[global.genes['synthesis']] : 1;
@@ -12915,7 +12929,7 @@ function diffCalc(res,period){
 function steelCheck(){
     if (global.resource.Steel.display === false && Math.rand(0,1250) === 0){
         global.resource.Steel.display = true;
-        modRes('Steel',1);
+        modRes('Steel', 1, true);
         messageQueue(loc('steel_sample'),'info',false,['progress']);
     }
 }

src/actions.js
@@ -5,7 +5,7 @@ import { unlockAchieve, challengeIcon, alevel, universeAffix, checkAdept } from 
 import { races, traits, genus_def, neg_roll_traits, randomMinorTrait, cleanAddTrait, combineTraits, biomes, planetTraits, setJType, altRace, setTraitRank, setImitation, shapeShift, basicRace, fathomCheck, traitCostMod, renderSupernatural, blubberFill, traitRank } from './races.js';
 import { defineResources, unlockCrates, unlockContainers, crateValue, containerValue, galacticTrade, spatialReasoning, resource_values, initResourceTabs, marketItem, containerItem, tradeSummery, faithBonus, templePlasmidBonus, faithTempleCount } from './resources.js';
 import { loadFoundry, defineJobs, jobScale, workerScale, job_desc } from './jobs.js';
-import { loadIndustry, defineIndustry, nf_resources, gridDefs, addSmelter } from './industry.js';
+import { loadIndustry, defineIndustry, nf_resources, gridDefs, addSmelter, cancelRituals } from './industry.js';
 import { defineGovernment, defineGarrison, buildGarrison, commisionGarrison, foreignGov, armyRating, garrisonSize, govEffect } from './civics.js';
 import { spaceTech, interstellarTech, galaxyTech, incrementStruct, universe_affixes, renderSpace, piracy, fuel_adjust, isStargateOn } from './space.js';
 import { renderFortress, fortressTech, warlordSetup } from './portal.js';
@@ -7742,8 +7742,7 @@ export function checkCosts(costs){
                 return;
             }
             let f_res = res === 'Species' ? global.race.species : res;
-            let fail_max = global.resource[f_res].max >= 0 && testCost > global.resource[f_res].max ? true : false;
-            if (testCost > Number(global.resource[f_res].amount) + global.resource[f_res].diff || fail_max){
+            if (testCost > Number(global.resource[f_res].amount) || (global.resource[f_res].max >= 0 && testCost > global.resource[f_res].max)){
                 test = false;
                 return;
             }
@@ -7986,11 +7985,7 @@ export function orbitDecayed(){
             if (global.city['pylon']){
                 global.space['pylon'] = { count: Math.ceil(global.city.pylon.count / 2) };
             }
-            if (global.race['casting']){
-                Object.keys(global.race.casting).forEach(function (c){
-                    global.race.casting[c] = 0;
-                });
-            }
+            cancelRituals();
         }
 
         Object.keys(actions.city).forEach(function (k){

src/civics.js
@@ -1830,7 +1830,7 @@ function war_campaign(gov){
         if (global.resource.Money.display && gains.Money > 0){
             gains.Money = lootModify(gains.Money,gov);
             loot = loot + loc('civics_garrison_quant_money',[gains.Money]);
-            modRes('Money',gains.Money);
+            modRes('Money',gains.Money,true);
         }
 
         let payout = basic.concat(common,rare);
@@ -1838,7 +1838,7 @@ function war_campaign(gov){
             if (gains[res] > 0 && (global.resource[res].display || res === 'Steel' || res === 'Titanium')){
                 gains[res] = lootModify(gains[res],gov);
                 loot = loot + loc('civics_garrison_quant_res',[gains[res],global.resource[res].name]);
-                modRes(res,gains[res]);
+                modRes(res,gains[res],true);
                 if (res === 'Steel' || res === 'Titanium'){
                     global.resource[res].display = true;
                 }

src/functions.js
@@ -1,4 +1,4 @@
-import { global, save, message_logs, message_filters, webWorker, keyMultiplier, intervals, resizeGame, atrack, p_on, quantum_level } from './vars.js';
+import { global, save, message_logs, message_filters, webWorker, keyMultiplier, intervals, resizeGame, atrack, p_on, quantum_level, tmp_vars } from './vars.js';
 import { loc } from './locale.js';
 import { races, traits, genus_def, traitSkin, fathomCheck } from './races.js';
 import { actions, actionDesc } from './actions.js';
@@ -663,20 +663,33 @@ export function tagEvent(event, data){
     } catch (err){}
 }
 
-export function modRes(res,val,notrack,buffer){
+export function resetResBuffer(){
+    // During fastLoop, temporarily increase the maximum storage to avoid unfortunate cases where
+    // storage cannot be maximized as a result of consuming a resource after it is produced.
+    // The resource buffer is eliminated at the end of fastLoop.
+    Object.keys(tmp_vars.resource).forEach(function (res) {
+        let temp_max = global.resource[res].max;
+        // Don't change infinite storage (-1) into finite storage
+        if (temp_max > 0){
+            temp_max += global.resource[res].amount;
+        }
+        tmp_vars.resource[res].temp_max = temp_max;
+    });
+}
+
+export function modRes(res,val,notrack){
     if(res === 'Food' && global.race['fasting']){
         global.resource[res].amount = 0;
         return false;
     }
     let count = global.resource[res].amount + val;
     let success = true;
-    if (count > global.resource[res].max && global.resource[res].max != -1){
-        count = global.resource[res].max;
+    let max = notrack ? global.resource[res].max : tmp_vars.resource[res].temp_max;
+    if (count > max && max >= 0){
+        count = max;
     }
     else if (count < 0){
-        if (!buffer || (buffer && (count * -1) > buffer)){
-            success = false;
-        }
+        success = false;
         count = 0;
     }
     if (!Number.isNaN(count)){
@@ -685,6 +698,9 @@ export function modRes(res,val,notrack,buffer){
             global.resource[res].delta += val;
             if (res === 'Mana' && val > 0){
                 global.resource[res].gen_d += val;
+            }
+            else if (val < 0 && max >= 0){
+                tmp_vars.resource[res].temp_max = Math.max(0, tmp_vars.resource[res].temp_max + val);
             }
         }
     }
@@ -2603,6 +2619,30 @@ export function format_emblem(achieve,size,baseIcon,fool,universe){
     }
 
     return emblem;
+}
+
+export function binary_limit_test(f, start=1, startIsMin=true){
+    if (start !== Math.floor(start) || start < 1){ return 0; }
+
+    let num = start;
+    while (f(num)){ num *= 2; }
+
+    let test = Math.floor(num/2);
+    if (num === start){
+        num = 0;
+        if (startIsMin){ test = 0; }
+    }
+    else {
+        num = test;
+    }
+
+    while (test > 1){
+        test = Math.floor(test/2);
+        if (f(num + test)){
+            num += test;
+        }
+    }
+    return num;
 }
 
 export function fibonacci(num, memo){

src/industry.js
@@ -1,6 +1,6 @@
-import { global, keyMultiplier, sizeApproximation, p_on, support_on, quantum_level, callback_queue } from './vars.js';
+import { global, keyMultiplier, sizeApproximation, p_on, support_on, quantum_level, callback_queue, active_rituals } from './vars.js';
 import { loc } from './locale.js';
-import { vBind, popover, clearElement, powerGrid, easterEgg, trickOrTreat } from './functions.js';
+import { vBind, popover, clearElement, powerGrid, easterEgg, trickOrTreat, binary_limit_test } from './functions.js';
 import { actions, checkCityRequirements, checkPowerRequirements } from './actions.js';
 import { races, traits, fathomCheck } from './races.js';
 import { atomic_mass } from './resources.js';
@@ -1316,6 +1316,38 @@ function loadPylon(parent,bind){
     });
 }
 
+export const ritual_types = ['farmer','miner','lumberjack','science','factory','army','hunting','crafting'];
+export function setupRituals(define=false){
+    if (define){
+        global.race['casting'] = {
+            farmer: 0,
+            miner: 0,
+            lumberjack: 0,
+            science: 0,
+            factory: 0,
+            army: 0,
+            hunting: 0,
+            crafting: 0,
+            total: 0,
+        };
+    }
+
+    if (global.race['casting']){
+        ritual_types.forEach(function (c){
+            active_rituals[c] = global.race.casting[c];
+        });
+    }
+}
+
+export function cancelRituals(){
+    if (global.race['casting']){
+        Object.keys(global.race.casting).forEach(function (c){
+            global.race.casting[c] = 0;
+            active_rituals[c] = 0;
+        });
+    }
+}
+
 function loadQuarry(parent,bind){
     parent.append($(`<div>${loc('modal_quarry_ratio',[global.resource.Chrysotile.name])}</div>`));
 
@@ -1610,9 +1642,14 @@ export function replicator(res,pow){
     }
 }
 
-export function manaCost(spell,rate){
-    rate = typeof rate === 'undefined' ? 0.0025 : rate;
+export function manaCost(spell,rate=0.0025){
     return spell * ((1 + rate) ** spell - 1);
+}
+
+export function maxRitualNum(mana, time_multiplier=0.25, rate=0.0025){
+    return binary_limit_test(function(num){
+        return (manaCost(num, rate) * time_multiplier) <= mana;
+    });
 }
 
 function colorRange(num,max,invert){

src/races.js
@@ -1,4 +1,4 @@
-import { global, seededRandom, save, webWorker, power_generated, keyMultiplier, sizeApproximation } from './vars.js';
+import { global, seededRandom, save, webWorker, power_generated, keyMultiplier, sizeApproximation, active_rituals } from './vars.js';
 import { loc } from './locale.js';
 import { defineIndustry } from './industry.js';
 import { setJobName, jobScale, loadFoundry } from './jobs.js';
@@ -6715,8 +6715,8 @@ export function racialTrait(workers,type){
         if (global.race['witch_hunter']){
             modifier *= 0.75;
         }
-        if (global.race.hasOwnProperty('casting') && global.race.casting[type === 'hellArmy' ? 'army' : type]){
-            let boost = global.race.casting[type === 'hellArmy' ? 'army' : type];
+        if (global.race.hasOwnProperty('casting') && active_rituals[type === 'hellArmy' ? 'army' : type]){
+            let boost = active_rituals[type === 'hellArmy' ? 'army' : type];
             if (global.race['witch_hunter']){
                 modifier *= 1 + (boost / (boost + 75) * 2.5);
             }
@@ -6846,6 +6846,7 @@ function purgeLumber(){
     if (global.race['casting']){
         global.race.casting.total -= global.race.casting.lumberjack;
         global.race.casting.lumberjack = 0;
+        active_rituals.lumberjack = 0;
         defineIndustry();
     }
     if (global.city['s_alter']) {
@@ -7037,6 +7038,7 @@ function adjustFood() {
         if (!farmersEnabled) {
             global.race.casting.total -= global.race.casting.farmer;
             global.race.casting.farmer = 0;
+            active_rituals.farmer = 0;
         }
         defineIndustry();
     }
@@ -9256,7 +9258,7 @@ function psychicKill(parent){
                     global.stats.psykill++;
                     blubberFill(1);
                     if (global.race['anthropophagite']){
-                        modRes('Food', 10000 * traits.anthropophagite.vars()[0]);
+                        modRes('Food', 10000 * traits.anthropophagite.vars()[0], true);
                     }
                     if (global.stats.psykill === 10){
                         renderPsychicPowers();

src/resources.js
@@ -1,4 +1,4 @@
-import { global, tmp_vars, keyMultiplier, breakdown, sizeApproximation, p_on, support_on } from './vars.js';
+import { global, tmp_vars, keyMultiplier, breakdown, sizeApproximation, p_on, support_on, active_rituals } from './vars.js';
 import { vBind, clearElement, modRes, flib, calc_mastery, calcPillar, eventActive, easterEgg, trickOrTreat, popover, harmonyEffect, darkEffect, hoovedRename, messageQueue } from './functions.js';
 import { traits, fathomCheck } from './races.js';
 import { templeCount, actions } from './actions.js';
@@ -423,9 +423,10 @@ export const craftingRatio = (function(){
                     auto: 1 + (govEffect.socialist()[0] / 100)
                 });
             }
-            if (global.race['casting'] && global.race.casting['crafting']){
-                let boost_m = 1 + (global.race.casting['crafting'] / (global.race.casting['crafting'] + 75));
-                let boost_a = 1 + (2 * global.race.casting['crafting'] / (2 * global.race.casting['crafting'] + 75));
+            if (global.race['casting'] && active_rituals['crafting']){
+                let num_rituals = active_rituals['crafting'];
+                let boost_m = 1 + (num_rituals / (num_rituals + 75));
+                let boost_a = 1 + (2 * num_rituals / (2 * num_rituals + 75));
                 crafting.general.multi.push({
                     name: loc(`modal_pylon_casting`),
                     manual: boost_m,
@@ -1031,7 +1032,8 @@ function loadResource(name,wiki,max,rate,tradable,stackable,color){
     tmp_vars.resource[name] = {
         color: color,
         tradable: tradable,
-        stackable: stackable
+        stackable: stackable,
+        temp_max: 0
     };
 }
 
@@ -2421,7 +2423,7 @@ function buildCrate(num){
         keyMutipler = Math.floor(global.resource[material].amount / cost);
     }
     if (global.resource[material].amount >= (cost * keyMutipler) && global.resource.Crates.amount < global.resource.Crates.max){
-        modRes(material,-(cost * keyMutipler));
+        modRes(material, -(cost * keyMutipler), true);
         global.resource.Crates.amount += keyMutipler;
     }
 }
@@ -2435,7 +2437,7 @@ function buildContainer(num){
         keyMutipler = Math.floor(global.resource['Steel'].amount / 125);
     }
     if (global.resource['Steel'].amount >= (125 * keyMutipler) && global.resource.Containers.amount < global.resource.Containers.max){
-        modRes('Steel',-(125 * keyMutipler));
+        modRes('Steel', -(125 * keyMutipler), true);
         global.resource.Containers.amount += keyMutipler;
     }
 }

commit a2d3dd240d82824dcbe6956010f971608bd30e14

Fix priority between sequencing and building queue
Resolves an issue introduced by #1468. Although priority inversion
between the research queue and gene assembly remains resolved, a new
priority inversion issue between the building queue and gene assembly
was created.

This commit also fixes some logic associated with "No Queue Order" ARPA.

src/arpa.js
@@ -2598,6 +2598,9 @@ export function buildArpa(pro,num,update,queue){
                 drawTech();
             }
         }
+        else {
+            break;
+        }
     }
     if (update){
         let amounts = [1,10,25,100];

src/main.js
@@ -8132,6 +8132,7 @@ function fastLoop(){
 
 function midLoop(){
     const astroSign = astrologySign();
+    let blockGeneBuffer = false;
     if (global.race.species === 'protoplasm'){
         let base = 100;
         if (global.stats.achieve['mass_extinction'] && global.stats.achieve['mass_extinction'].l > 1){
@@ -11068,11 +11069,10 @@ function midLoop(){
             global.race.casting.total = total;
         }
 
-        let blockGeneBuffer = false;
         if (global.tech['r_queue'] && global.r_queue.display){
             let idx = -1;
             let c_action = false;
-            let stop = false;
+            let stop = global.r_queue.pause;
             let time = 0; let untime = 0;
             let spent = { t: {t:0,rt:0}, r: {}, rr: {}, id: {}};
             for (let i=0; i<global.r_queue.queue.length; i++){
@@ -11162,10 +11162,6 @@ function midLoop(){
             }
         }
 
-        if (global.arpa.sequence && global.arpa.sequence['auto'] && global.tech['genetics'] && global.tech['genetics'] >= 8){
-            buildGene(blockGeneBuffer);
-        }
-
         if (p_on['soul_forge']){
             vBind({el: `#fort`},'update');
         }
@@ -11174,15 +11170,16 @@ function midLoop(){
     }
 
     if (global.tech['queue'] && global.queue.display){
+        const qAny = global.settings.qAny ? true : false; // Coerce to boolean and cache to avoid repeated Vue callbacks.
         let idx = -1;
         let c_action = false;
-        let stop = false;
+        let stop = global.queue.pause;
         let deepScan = ['space','interstellar','galaxy','portal','tauceti','eden'];
         let time = 0;
         let spent = { t: {t:0,rt:0}, r: {}, rr: {}, id: {}};
         let arpa = false;
         for (let i=0; i<global.queue.queue.length; i++){
-            if (global.settings.qAny){
+            if (qAny){
                 spent = { t: {t:0,rt:0}, r: {}, rr: {}, id: {}};
                 time = 0;
             }
@@ -11243,15 +11240,26 @@ function midLoop(){
                     struct['time'] = -1;
                 }
 
-                if (arpaTimeCheck(t_action, 0.01) >= 0){
-                    if (global.settings.qAny && !global.queue.pause && struct['time'] > 1){
-                        buildArpa(struct.type,100,true);
-                    }
-                    else if (!stop){
+                if (!stop){
+                    const segment_info = arpaTimeCheck(t_action, 0.01, false, true);
+                    const segment_time = segment_info.t;
+
+                    // Ordered building queue: always stop on the first struct or project.
+                    // Unordered building queue: entire remainder of ARPA project can be completed now
+                    if (!qAny || struct['time'] === 0){
                         c_action = t_action;
                         idx = i;
                         arpa = true;
                         stop = true;
+                    }
+                    // Unordered building queue: at least 1% of ARPA project can be completed now
+                    else if (qAny && segment_time === 0){
+                        // Build in parallel without blocking later structs or projects from also making progress
+                        buildArpa(struct.type, 100, true);
+                    }
+                    // If blocking resource is Knowledge and segment time remaining is less than 1 second, then block gene assembly
+                    if (segment_info.r === 'Knowledge' && segment_time > 0 && segment_time <= 1){
+                        blockGeneBuffer = true;
                     }
                 }
             }
@@ -11265,14 +11273,17 @@ function midLoop(){
                             c_action = t_action;
                             idx = i;
                             arpa = false;
-                            if (global.settings.qAny){
+                            if (qAny){
                                 stop = true;
                             }
                         }
                         else {
+                            if (!stop && t_time <= 1 && t_action.cost.hasOwnProperty('Knowledge')){
+                                blockGeneBuffer = true;
+                            }
                             time += t_time;
                         }
-                        if (!global.settings.qAny){
+                        if (!qAny){
                             stop = true;
                         }
                         struct['time'] = time;
@@ -11294,7 +11305,7 @@ function midLoop(){
                     struct['time'] = -1;
                 }
             }
-            struct.qa = global.settings.qAny ? true : false;
+            struct.qa = qAny;
         }
         if (idx >= 0 && c_action && !global.queue.pause){
             let triggerd = false;
@@ -11410,6 +11421,10 @@ function midLoop(){
             global.queue.queue = merged_queue;
             buildQueue();
         }
+    }
+
+    if (global?.arpa?.sequence?.auto && global.tech?.genetics >= 8){
+        buildGene(blockGeneBuffer);
     }
 
     resourceAlt();