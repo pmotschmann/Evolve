9b62508f4514ac990d4bc977a151bbb2e41ab3cc
yarukishi <150128243+yarukishi@users.noreply.github.com>
2025-10-05 15:17:21 +0900

    Don't try to make custom race lab twice

--- src/space.js
+++ src/space.js
@@ -7871,6 +7871,9 @@ export function ascendLab(hybrid,wiki){
         global.settings.spaceTabs = 0;
     }
 
+    if (document.getElementById('celestialLab')){
+        return;
+    }
     let unlockedTraits = {};
     let lab = $(`<div id="celestialLab" class="celestialLab"></div>`);
 
