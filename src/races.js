dda325753a591961879751e2f97c8088bcbbfcf1
AERobert <67764127+AERobert@users.noreply.github.com>
2026-07-31 22:35:37 -0400

    Fix Typos (#1528)
    * fix typos in strings.json
    
    * cheese_gruyeye: Fix spelling of "Gruyèye" to "Gruyère".
    * event_rumor_type1: Changed "towns'" to "town's" since it's singular.
    * evo_challenge_orbit_decay_effect: Changed "homeworlds'" to "homeworld's" since it's singular.
    * city_temple_desc_evil: Fixed spelling "propogranda" to "propaganda".
    * tech_mock_battles_effect: Fix spelling "massacure" to "massacre" and changed "is" to "are" to agree with "battles". Fixes #1496
    * tech_reapers_effect: Fixed spelling "harrassing" to "harassing".
    * achieve_extinct_beholder_desc: Changed "a Eye-spector" to "an Eye-spector". Fixes #1495
    * achieve_extinct_bombardier_flair: Changed a repeated word "out out" to "out of".
    * governor_bureaucrat_desc: Made "bureaucrats" plural.
    * gov_trait_dirty_jobs_desc: Fixed spelling "benefical" to "beneficial".
    * portal_mech_chassis_hound_desc: Fixed spelling "usualy" to "usually".
    * portal_mech_chassis_harpy_desc: Fixed spelling "moble" to "mobile".
    * portal_mech_weapon_flesh_desc: Fixed spelling "typicaly" to "typically".
    * eden_asphodel_desc: Fixed spelling "mediocracy" to "mediocrity".
    * tau_gas2_desc: Changed "it still" to "it's still".
    * wiki_faq_q_quantum: Changed "effect" to "affect" since the question is addressing what is influenced.
    * wiki_basics_prehistoric_para6: Changed "effect" to "affect" since the text is using it as a verb.
    * wiki_trait_effect_instinct: Changed "is" to "are" to agree with "losses".
    * wiki_tech_rebar: Changed "is" to "are" to agree with "costs".
    * wiki_mechanics_custom_para6: Fixed spelling "taxonimized" to "taxonomized".
    * wiki_mechanics_seed_para1: Removed repeated "that".
    * wiki_mechanics_thralls_para6: Changed "tormenters" to "tormentors" to be consistent with the rest of the game.
    * wiki_challenges_scenarios_truepath_rival_para4: Changed "thats hurt some production" to "that hurts some production".
    * wiki_resets_mad_para2: Change repeated "it" to "It is".
    * wiki_hell_minion_para5: Fix spelling "minon" to "minion".
    * wiki_planet_geology_para1: Change "effect" to "affect" since the word is being used as a verb.
    * wiki_events_pet_action_para7: Fix spelling "sucessfully" to "successfully".
    * wiki_events_pet_action_para9: Fix spelling "sucessfully" to "successfully".
    * wiki_events_progress_intro_para1: Change "these type of events" to just "these events".
    * wiki_events_xmas_para1: Change "an special holiday gift" to "a special holiday gift".
    
    * fix driod -> droid typo in aria text
    
    The typo was in the aria-label describing the number of mining droids assigned to a given resource.
    
    * remove extraneous 'a' from README
    
    The typo is in the first sentence of the README's section "Contributing a Language file, "If you are interested in a contributing a new language..."
    
    * fix decresae in psychic labels

--- src/races.js
+++ src/races.js
@@ -9211,7 +9211,7 @@ function psychicBoost(parent){
     if (global.tech.psychic >= 4){
         let channel = $(`<div class="gap">${loc('psychic_channel')}</div>`);
         let psy = $(`<span class="current">{{ c.boost }}</span>`);
-        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decresae Energy reserved for ${loc(`psychic_attack`)}"><span>&laquo;</span></span>`);
+        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decrease Energy reserved for ${loc(`psychic_attack`)}"><span>&laquo;</span></span>`);
         let add = $(`<span role="button" class="add" @click="add" aria-label="Increase Energy reserved for ${loc(`psychic_attack`)}"><span>&raquo;</span></span>`);
         channel.append(sub);
         channel.append(psy);
@@ -9331,7 +9331,7 @@ function psychicAssault(parent){
     if (global.tech.psychic >= 4){
         let channel = $(`<div class="gap">${loc('psychic_channel')}</div>`);
         let psy = $(`<span class="current">{{ assault }}</span>`);
-        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decresae Energy reserved for ${loc(`psychic_attack`)}"><span>&laquo;</span></span>`);
+        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decrease Energy reserved for ${loc(`psychic_attack`)}"><span>&laquo;</span></span>`);
         let add = $(`<span role="button" class="add" @click="add" aria-label="Increase Energy reserved for ${loc(`psychic_attack`)}"><span>&raquo;</span></span>`);
         channel.append(sub);
         channel.append(psy);
@@ -9401,7 +9401,7 @@ function psychicFinance(parent){
     if (global.tech.psychic >= 4){
         let channel = $(`<div class="gap">${loc('psychic_channel')}</div>`);
         let psy = $(`<span class="current">{{ cash }}</span>`);
-        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decresae Energy reserved for ${loc(`psychic_profit`)}"><span>&laquo;</span></span>`);
+        let sub = $(`<span role="button" class="sub" @click="sub" aria-label="Decrease Energy reserved for ${loc(`psychic_profit`)}"><span>&laquo;</span></span>`);
         let add = $(`<span role="button" class="add" @click="add" aria-label="Increase Energy reserved for ${loc(`psychic_profit`)}"><span>&raquo;</span></span>`);
         channel.append(sub);
         channel.append(psy);
